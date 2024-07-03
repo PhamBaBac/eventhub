21852b1e7962173d39c33a2a4e40f0e11f7cbe34
PhamBaBac <bacphamba082@gmail.com>
2024-07-03 09:56:03 +0700

    create Drawer Navigation

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,23 +1,109 @@
-import {View, Text, Button} from 'react-native';
+import { HambergerMenu, Notification, SearchNormal, Sort } from 'iconsax-react-native';
 import React from 'react';
-import {useDispatch} from 'react-redux';
-import {removeAuth} from '../../redux/reducers/authReducer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import { Platform, StatusBar, TouchableOpacity, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { RowComponent, SpaceComponent, TextComponent } from '../../components';
+import CircleComponent from '../../components/CircleComponent';
+import { appColors, fontFamilies } from '../../constants';
+import { globalStyles } from '../../styles/globalStyles';
 
-const HomeScreen = () => {
-  const dispatch = useDispatch();
+const HomeScreen = ({navigation}: any) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Log out"
-        onPress={async () => {
-          await AsyncStorage.clear();
-          await GoogleSignin.signOut();
-          dispatch(removeAuth({}));
-        }}
-      />
+    <View style={[globalStyles.container]}>
+      <StatusBar barStyle={'light-content'} />
+      <View
+        style={{
+          backgroundColor: appColors.primary,
+          height: 178 + (Platform.OS === 'ios' ? 16 : 0),
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40,
+          paddingTop:
+            Platform.OS === 'android'
+              ? StatusBar.currentHeight
+                ? StatusBar.currentHeight + 12
+                : 12
+              : 52,
+        }}>
+        <View style={{paddingHorizontal: 16}}>
+          <RowComponent>
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+              <HambergerMenu size={24} color={appColors.white} />
+            </TouchableOpacity>
+            <View style={[{flex: 1, alignItems: 'center'}]}>
+              <RowComponent>
+                <TextComponent
+                  text="Current Location"
+                  color={appColors.white2}
+                  size={12}
+                />
+                <MaterialIcons
+                  name="arrow-drop-down"
+                  size={18}
+                  color={appColors.white}
+                />
+              </RowComponent>
+              <TextComponent
+                text="New York, USA"
+                flex={0}
+                color={appColors.white}
+                font={fontFamilies.medium}
+                size={13}
+              />
+            </View>
+            <CircleComponent color="#524CE0" size={36}>
+              <View>
+                <Notification size={18} color={appColors.white} />
+                <View
+                  style={{
+                    backgroundColor: '#02E9FE',
+                    width: 10,
+                    height: 10,
+                    borderRadius: 4,
+                    borderWidth: 2,
+                    borderColor: '#524CE0',
+                    position: 'absolute',
+                    top: -2,
+                    right: -2,
+                  }}
+                />
+              </View>
+            </CircleComponent>
+          </RowComponent>
+          <SpaceComponent height={18} />
+          <RowComponent>
+            <RowComponent styles={{flex: 1}}>
+              <SearchNormal
+                variant="TwoTone"
+                size={24}
+                color={appColors.white}
+              />
+              <View
+                style={{
+                  width: 1,
+                  height: 18,
+                  marginHorizontal: 12,
+                  backgroundColor: '#A29EF0',
+                }}
+              />
+              <TextComponent text="Search..." color={`#A29EF0`} flex={1} />
+            </RowComponent>
+            <RowComponent
+              styles={{
+                backgroundColor: '#5D56F3',
+                paddingHorizontal: 12,
+                paddingVertical: 8,
+                borderRadius: 100,
+              }}>
+              <CircleComponent size={19.3} color={`#A29EF0`}>
+                <Sort size={12} color={appColors.primary} />
+              </CircleComponent>
+              <SpaceComponent width={8} />
+              <TextComponent text="Filters" color={appColors.white} />
+            </RowComponent>
+          </RowComponent>
+        </View>
+      </View>
+      <View style={{flex: 1}}></View>
     </View>
   );
 };
